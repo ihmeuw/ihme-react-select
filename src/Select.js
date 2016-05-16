cce4d01ee28408e661abd9599707b92aab7c1569
Gabe Medrash <gmedrash@gmail.com>
2016-05-16 13:41:18 -0700

    stop filtering options when multi=true

--- src/Select.js
+++ src/Select.js
@@ -869,7 +869,7 @@ const Select = React.createClass({
 
 	render () {
 		let valueArray = this.getValueArray(this.props.value);
-		let options = this._visibleOptions = this.filterOptions(this.props.multi ? valueArray : null);
+		let options = this._visibleOptions = this.filterOptions(null);
 		let isOpen = this.state.isOpen;
 		if (this.props.multi && !options.length && valueArray.length && !this.state.inputValue) isOpen = false;
 		let focusedOption = this._focusedOption = this.getFocusableOption(valueArray[0]);
