d32b224dc2c5adfe95f6d36b27fc7dddd5ab307d
Gabe Medrash <gmedrash@gmail.com>
2016-05-17 08:42:27 -0700

    v1.2.1

--- examples/dist/standalone.js
+++ examples/dist/standalone.js
@@ -378,77 +378,80 @@ function stringifyValue(value) {
 
 var stringOrNode = _react2['default'].PropTypes.oneOfType([_react2['default'].PropTypes.string, _react2['default'].PropTypes.node]);
 
+var propTypes = {
+	addLabelText: _react2['default'].PropTypes.string, // placeholder displayed when you want to add a label on a multi-value input
+	allowCreate: _react2['default'].PropTypes.bool, // whether to allow creation of new entries
+	arrowComponent: _react2['default'].PropTypes.func, // customized arrow component to render
+	autoBlur: _react2['default'].PropTypes.bool, // automatically blur the component when an option is selected
+	autofocus: _react2['default'].PropTypes.bool, // autofocus the component on mount
+	autosize: _react2['default'].PropTypes.bool, // whether to enable autosizing or not
+	backspaceRemoves: _react2['default'].PropTypes.bool, // whether backspace removes an item if there is no text input
+	className: _react2['default'].PropTypes.string, // className for the outer element
+	clearAllText: stringOrNode, // title for the "clear" control when multi: true
+	clearValueText: stringOrNode, // title for the "clear" control
+	clearable: _react2['default'].PropTypes.bool, // should it be possible to reset value
+	delimiter: _react2['default'].PropTypes.string, // delimiter to use to join multiple values for the hidden field value
+	disabled: _react2['default'].PropTypes.bool, // whether the Select is disabled or not
+	escapeClearsValue: _react2['default'].PropTypes.bool, // whether escape clears the value when the menu is closed
+	filterOption: _react2['default'].PropTypes.func, // method to filter a single option (option, filterString)
+	filterOptions: _react2['default'].PropTypes.any, // boolean to enable default filtering or function to filter the options array ([options], filterString, [values])
+	hierarchical: _react2['default'].PropTypes.bool, // whether to render options as hierarchical list
+	ignoreAccents: _react2['default'].PropTypes.bool, // whether to strip diacritics when filtering
+	ignoreCase: _react2['default'].PropTypes.bool, // whether to perform case-insensitive filtering
+	inputProps: _react2['default'].PropTypes.object, // custom attributes for the Input
+	inputRenderer: _react2['default'].PropTypes.func, // returns a custom input component
+	isLoading: _react2['default'].PropTypes.bool, // whether the Select is loading externally or not (such as options being loaded)
+	joinValues: _react2['default'].PropTypes.bool, // joins multiple values into a single form field with the delimiter (legacy mode)
+	labelKey: _react2['default'].PropTypes.string, // path of the label value in option objects
+	matchPos: _react2['default'].PropTypes.string, // (any|start) match the start or entire string when filtering
+	matchProp: _react2['default'].PropTypes.string, // (any|label|value) which option property to filter on
+	menuBuffer: _react2['default'].PropTypes.number, // optional buffer (in px) between the bottom of the viewport and the bottom of the menu
+	menuContainerStyle: _react2['default'].PropTypes.object, // optional style to apply to the menu container
+	menuRenderer: _react2['default'].PropTypes.func, // renders a custom menu with options
+	menuStyle: _react2['default'].PropTypes.object, // optional style to apply to the menu
+	multi: _react2['default'].PropTypes.bool, // multi-value input
+	name: _react2['default'].PropTypes.string, // generates a hidden <input /> tag with this field name for html forms
+	newOptionCreator: _react2['default'].PropTypes.func, // factory to create new options when allowCreate set
+	noResultsText: stringOrNode, // placeholder displayed when there are no matching search results
+	onBlur: _react2['default'].PropTypes.func, // onBlur handler: function (event) {}
+	onBlurResetsInput: _react2['default'].PropTypes.bool, // whether input is cleared on blur
+	onChange: _react2['default'].PropTypes.func, // onChange handler: function (newValue) {}
+	onClose: _react2['default'].PropTypes.func, // fires when the menu is closed
+	onFocus: _react2['default'].PropTypes.func, // onFocus handler: function (event) {}
+	onInputChange: _react2['default'].PropTypes.func, // onInputChange handler: function (inputValue) {}
+	onMenuScrollToBottom: _react2['default'].PropTypes.func, // fires when the menu is scrolled to the bottom; can be used to paginate options
+	onOpen: _react2['default'].PropTypes.func, // fires when the menu is opened
+	onValueRemove: _react2['default'].PropTypes.func, // onRemove handler for valueComponent: function(value, event) {}
+	onValueClick: _react2['default'].PropTypes.func, // onClick handler for value labels: function (value, event) {}
+	openAfterFocus: _react2['default'].PropTypes.bool, // boolean to enable opening dropdown when focused
+	openOnFocus: _react2['default'].PropTypes.bool, // always open options menu on focus
+	optionClassName: _react2['default'].PropTypes.string, // additional class(es) to apply to the <Option /> elements
+	optionComponent: _react2['default'].PropTypes.func, // option component to render in dropdown
+	optionRenderer: _react2['default'].PropTypes.func, // optionRenderer: function (option) {}
+	options: _react2['default'].PropTypes.array, // array of options
+	placeholder: stringOrNode, // field placeholder, displayed when there's no value
+	required: _react2['default'].PropTypes.bool, // applies HTML5 required attribute when needed
+	resetValue: _react2['default'].PropTypes.any, // value to use when you clear the control
+	scrollMenuIntoView: _react2['default'].PropTypes.bool, // boolean to enable the viewport to shift so that the full menu fully visible when engaged
+	searchable: _react2['default'].PropTypes.bool, // whether to enable searching feature or not
+	simpleValue: _react2['default'].PropTypes.bool, // pass the value to onChange as a simple value (legacy pre 1.0 mode), defaults to false
+	style: _react2['default'].PropTypes.object, // optional style to apply to the control
+	tabIndex: _react2['default'].PropTypes.string, // optional tab index of the control
+	tabSelectsValue: _react2['default'].PropTypes.bool, // whether to treat tabbing out while focused to be value selection
+	value: _react2['default'].PropTypes.any, // initial field value
+	valueComponent: _react2['default'].PropTypes.func, // value component to render
+	valueKey: _react2['default'].PropTypes.string, // path of the label value in option objects
+	valueRenderer: _react2['default'].PropTypes.func, // valueRenderer: function (option) {}
+	wrapperStyle: _react2['default'].PropTypes.object };
+
+exports.propTypes = propTypes;
+// optional style to apply to the component wrapper
 var Select = _react2['default'].createClass({
 
 	displayName: 'Select',
 
-	propTypes: {
-		addLabelText: _react2['default'].PropTypes.string, // placeholder displayed when you want to add a label on a multi-value input
-		allowCreate: _react2['default'].PropTypes.bool, // whether to allow creation of new entries
-		arrowComponent: _react2['default'].PropTypes.func, // customized arrow component to render
-		autoBlur: _react2['default'].PropTypes.bool, // automatically blur the component when an option is selected
-		autofocus: _react2['default'].PropTypes.bool, // autofocus the component on mount
-		autosize: _react2['default'].PropTypes.bool, // whether to enable autosizing or not
-		backspaceRemoves: _react2['default'].PropTypes.bool, // whether backspace removes an item if there is no text input
-		className: _react2['default'].PropTypes.string, // className for the outer element
-		clearAllText: stringOrNode, // title for the "clear" control when multi: true
-		clearValueText: stringOrNode, // title for the "clear" control
-		clearable: _react2['default'].PropTypes.bool, // should it be possible to reset value
-		delimiter: _react2['default'].PropTypes.string, // delimiter to use to join multiple values for the hidden field value
-		disabled: _react2['default'].PropTypes.bool, // whether the Select is disabled or not
-		escapeClearsValue: _react2['default'].PropTypes.bool, // whether escape clears the value when the menu is closed
-		filterOption: _react2['default'].PropTypes.func, // method to filter a single option (option, filterString)
-		filterOptions: _react2['default'].PropTypes.any, // boolean to enable default filtering or function to filter the options array ([options], filterString, [values])
-		hierarchical: _react2['default'].PropTypes.bool, // whether to render options as hierarchical list
-		ignoreAccents: _react2['default'].PropTypes.bool, // whether to strip diacritics when filtering
-		ignoreCase: _react2['default'].PropTypes.bool, // whether to perform case-insensitive filtering
-		inputProps: _react2['default'].PropTypes.object, // custom attributes for the Input
-		inputRenderer: _react2['default'].PropTypes.func, // returns a custom input component
-		isLoading: _react2['default'].PropTypes.bool, // whether the Select is loading externally or not (such as options being loaded)
-		joinValues: _react2['default'].PropTypes.bool, // joins multiple values into a single form field with the delimiter (legacy mode)
-		labelKey: _react2['default'].PropTypes.string, // path of the label value in option objects
-		matchPos: _react2['default'].PropTypes.string, // (any|start) match the start or entire string when filtering
-		matchProp: _react2['default'].PropTypes.string, // (any|label|value) which option property to filter on
-		menuBuffer: _react2['default'].PropTypes.number, // optional buffer (in px) between the bottom of the viewport and the bottom of the menu
-		menuContainerStyle: _react2['default'].PropTypes.object, // optional style to apply to the menu container
-		menuRenderer: _react2['default'].PropTypes.func, // renders a custom menu with options
-		menuStyle: _react2['default'].PropTypes.object, // optional style to apply to the menu
-		multi: _react2['default'].PropTypes.bool, // multi-value input
-		name: _react2['default'].PropTypes.string, // generates a hidden <input /> tag with this field name for html forms
-		newOptionCreator: _react2['default'].PropTypes.func, // factory to create new options when allowCreate set
-		noResultsText: stringOrNode, // placeholder displayed when there are no matching search results
-		onBlur: _react2['default'].PropTypes.func, // onBlur handler: function (event) {}
-		onBlurResetsInput: _react2['default'].PropTypes.bool, // whether input is cleared on blur
-		onChange: _react2['default'].PropTypes.func, // onChange handler: function (newValue) {}
-		onClose: _react2['default'].PropTypes.func, // fires when the menu is closed
-		onFocus: _react2['default'].PropTypes.func, // onFocus handler: function (event) {}
-		onInputChange: _react2['default'].PropTypes.func, // onInputChange handler: function (inputValue) {}
-		onMenuScrollToBottom: _react2['default'].PropTypes.func, // fires when the menu is scrolled to the bottom; can be used to paginate options
-		onOpen: _react2['default'].PropTypes.func, // fires when the menu is opened
-		onValueRemove: _react2['default'].PropTypes.func, // onRemove handler for valueComponent: function(value, event) {}
-		onValueClick: _react2['default'].PropTypes.func, // onClick handler for value labels: function (value, event) {}
-		openAfterFocus: _react2['default'].PropTypes.bool, // boolean to enable opening dropdown when focused
-		openOnFocus: _react2['default'].PropTypes.bool, // always open options menu on focus
-		optionClassName: _react2['default'].PropTypes.string, // additional class(es) to apply to the <Option /> elements
-		optionComponent: _react2['default'].PropTypes.func, // option component to render in dropdown
-		optionRenderer: _react2['default'].PropTypes.func, // optionRenderer: function (option) {}
-		options: _react2['default'].PropTypes.array, // array of options
-		placeholder: stringOrNode, // field placeholder, displayed when there's no value
-		required: _react2['default'].PropTypes.bool, // applies HTML5 required attribute when needed
-		resetValue: _react2['default'].PropTypes.any, // value to use when you clear the control
-		scrollMenuIntoView: _react2['default'].PropTypes.bool, // boolean to enable the viewport to shift so that the full menu fully visible when engaged
-		searchable: _react2['default'].PropTypes.bool, // whether to enable searching feature or not
-		simpleValue: _react2['default'].PropTypes.bool, // pass the value to onChange as a simple value (legacy pre 1.0 mode), defaults to false
-		style: _react2['default'].PropTypes.object, // optional style to apply to the control
-		tabIndex: _react2['default'].PropTypes.string, // optional tab index of the control
-		tabSelectsValue: _react2['default'].PropTypes.bool, // whether to treat tabbing out while focused to be value selection
-		value: _react2['default'].PropTypes.any, // initial field value
-		valueComponent: _react2['default'].PropTypes.func, // value component to render
-		valueKey: _react2['default'].PropTypes.string, // path of the label value in option objects
-		valueRenderer: _react2['default'].PropTypes.func, // valueRenderer: function (option) {}
-		wrapperStyle: _react2['default'].PropTypes.object },
+	propTypes: propTypes,
 
-	// optional style to apply to the component wrapper
 	statics: { Async: _Async2['default'] },
 
 	getDefaultProps: function getDefaultProps() {
@@ -1305,7 +1308,6 @@ var Select = _react2['default'].createClass({
 });
 
 exports['default'] = Select;
-module.exports = exports['default'];
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"./Arrow":1,"./Async":2,"./Option":3,"./Value":5,"./utils/stripDiacritics":6}],5:[function(require,module,exports){
